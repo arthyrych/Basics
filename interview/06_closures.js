a0327ecd17b5bcb22180720594b9595ac69dd228
arthyrych <arthyrych@gmail.com>
2022-07-12 22:20:18 +0300

    iife covered

--- interview/06_closures.js
+++ interview/06_closures.js
@@ -1,5 +1,7 @@
-// closures give an access to a function to variables from the outer scope (function inside function)
+// closure gives you access to an outer function's scope from an inner function (function inside function)
+// we have no direct access to the enclosed variable
 
+// variable message is enclosed and we have no direct access to it except through log method used by inner function
 function sayHelloTo(name) {
   const message = 'Hello ' + name
   return function() {
@@ -11,6 +13,8 @@ const helloToJoe = sayHelloTo('Joe')
 console.log(helloToJoe) // Hello Joe
 console.log(helloToJoe()) // undefined
 
+
+// variable fw is enclosed and we have no direct access to it except through the object's methods
 function createFrameworkManager() {
   const fw = ['Cypress', 'Playwright']
   return {
---
+++ interview/07_iife.js
@@ -0,0 +1,28 @@
+// immediately invoked function expression
+// functions in () and invoked immediately
+
+// issue
+let result = []
+for (var i = 0; i < 5; i++) {
+  result.push(function() {
+    console.log(i)
+  })
+}
+
+result[1]() // 5
+result[2]() // 5
+
+
+// fixed with iife
+let result2 = []
+for (var i =0; i < 5; i++) {
+  (function() {
+    var j = i
+    result2.push(function() {
+      console.log(j)
+    })
+  })()
+}
+
+result2[1]() // 1
+result2[2]() // 2
