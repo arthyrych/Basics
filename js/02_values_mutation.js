dea46968d9e87bd7baff5d757c1febc992040cfb
arthyrych <arthyrych@gmail.com>
2023-12-07 21:55:12 +0200

    updated arrays

--- js/02_values_mutation.js
+++ js/02_values_mutation.js
@@ -11,26 +11,26 @@ console.log('b', b) // b 43
 
 
 // non-primitive (objects, arrays, functions) data types passed by reference
-let arr1 = [1, 2, 3]
+let array1 = [1, 2, 3]
 // it works as a link to the same location in memory
-let arr2 = arr1
-// actions with arr2 will affect arr1 and vice versa
-arr2.push(4)
-console.log('arr1', arr1) // arr1 [ 1, 2, 3, 4 ]
-console.log('arr2', arr2) // arr2 [ 1, 2, 3, 4 ]
+let array2 = array1
+// actions with array2 will affect array1 and vice versa
+array2.push(4)
+console.log('array1', array1) // array1 [ 1, 2, 3, 4 ]
+console.log('array2', array2) // array2 [ 1, 2, 3, 4 ]
 
 
 // in order to avoid this kind of mutation we need to return a copy of the array and work with it
-let arr3 = [1, 2]
+let array3 = [1, 2]
 // concat method without parameters returns the copy of an array
-let arr4 = arr3.concat()
-// actions with arr2 will NOT affect arr1
-arr4.push(3)
-console.log('arr3', arr3) // arr3 [ 1, 2 ]
-console.log('arr4', arr4) // arr4 [ 1, 2, 3 ]
+let array4 = array3.concat()
+// actions with array2 will NOT affect array1
+array4.push(3)
+console.log('array3', array3) // array3 [ 1, 2 ]
+console.log('array4', array4) // array4 [ 1, 2, 3 ]
 
 
 // a link equals to its origin
 // a copy doesn't equal to its origin
-console.log(arr1 === arr2) // true
-console.log(arr3 === arr4) // false
+console.log(array1 === array2) // true
+console.log(array3 === array4) // false
