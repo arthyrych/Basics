50b05db7fcaebc6425d0d131662b09f7f7dce8d2
arthyrych <arthyrych@gmail.com>
2023-12-11 21:20:56 +0200

    updated array and object methods

--- js/14_array_object_methods.js
+++ js/14_array_object_methods.js
@@ -1,3 +1,34 @@
-// TBD
-// array - reduce, sort, find, filter, map, forEach, push, pop, shift, unshift, join
-// object - freeze
+// - Array
+
+// -- array.push(elements) - adds elements to the end and returns the modified array
+// -- array.pop() - removes and returns the last element, modifying the array
+// -- array.unshift(elements) - adds elements to the beginning and returns the modified array
+// -- array.shift() - removes and returns the first element, modifying the array
+
+// -- array.map(num => num * num) - transforms each element, creates and returns a new array with the results
+// -- array.forEach(num => num * num) - performs actions on each element without creating a new array
+
+// -- array.some(num => num % 2 === 0) - checks if at least one element satisfies the condition, returns true or false
+// -- array.every(num => num % 2 === 0) - checks if all elements satisfy the condition, returns true or false
+
+// -- array.find(num => num > 10) - returns the first element in an array that satisfies a condition
+// -- array.filter(num => num > 10) - returns all elements in an array that satisfy a condition
+
+// -- array.indexOf(element) - returns the index (position) of the first occurrence; otherwise returns -1
+// -- array.join() - creates and returns a string by concatenating array elements with commas or a specified separator
+// -- array.sort() - sorts the array in place and returns a reference to the same modified array
+
+// -- array.reduce((accumulator, currentValue) => accumulator + currentValue, 0) 
+// -- combines all elements of an array into a single value, often used for summing or aggregating
+
+
+// - Object 
+
+// -- object.hasOwnProperty(property) - returns true or false whether the object has the property as its own property (not inherited)
+
+// -- Object.keys(object) - returns an array of strings representing all the enumerable properties of an object
+// -- Object.values(object) - returns an array of values for each enumerable property of an object
+// -- Object.entries(object) - returns an array of [key, value] pairs for each enumerable property of an object
+// -- Object.assign({}, targetObj, sourceObj) - returns a new object with values copied from sourceObject to targetObject
+
+// -- Object.freeze(object) - returns the same object but immutable (prevents any changes to the object) 
