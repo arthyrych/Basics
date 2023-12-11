a15972ce663b7cf0425f90954484bfac6b53a6c1
arthyrych <arthyrych@gmail.com>
2023-12-11 14:02:55 +0200

    updated TAF layers and git questions

--- general/01_testing_theory.js
+++ general/01_testing_theory.js
@@ -1,22 +1,22 @@
 // Testing Theory
 
 // - Testing objectives
-// -- To provide sufficient information to stakeholders regarding the level of quality of the test object
-// -- To verify whether all specified requirements have been fulfilled (verification)
-// -- To check whether the test object is complete and if it works as the users and stakeholders expect (validation)
-// -- To prevent defects by evaluate work products such as requirements, user stories, design, and code
-// -- To find defects and failures thus reduce the level of risk of inadequate software quality
-// -- To build confidence in the level of quality of the test object
-// -- To comply with contractual, legal, or regulatory requirements or standards
+// 1) To provide sufficient information to stakeholders regarding the level of quality of the test object
+// 2) To verify whether all specified requirements have been fulfilled (verification)
+// 3) To check whether the test object is complete and if it works as the users and stakeholders expect (validation)
+// 4) To prevent defects by evaluate work products such as requirements, user stories, design, and code
+// 5) To find defects and failures thus reduce the level of risk of inadequate software quality
+// 6) To build confidence in the level of quality of the test object
+// 7) To comply with contractual, legal, or regulatory requirements or standards
 
 // - Testing principles
-// -- Testing shows the presence of defects, not their absence
-// -- Exhaustive testing is impossible
-// -- Early testing saves time and money
-// -- Defects cluster together
-// -- Beware of pesticide paradox
-// -- Testing is context dependent
-// -- Absence-of-errors is a fallacy (validation)
+// 1) Testing shows the presence of defects, not their absence
+// 2) Exhaustive testing is impossible
+// 3) Early testing saves time and money
+// 4) Defects cluster together
+// 5) Beware of pesticide paradox
+// 6) Testing is context dependent
+// 7) Absence-of-errors is a fallacy (validation)
 
 // - Testing process and SDLC
 // -- Monitoring and Control during all testing phases
@@ -24,11 +24,11 @@
 // -- SDLC - Planning - Analysis - Design - Development - Integration and Testing - Deployment - Maintenance
 
 // - Testing levels
-// -- Component/Unit/Module/Program testing
-// -- Component integration testing
-// -- System integration testing
-// -- System testing
-// -- Acceptance testing (User Acceptance Testing and Operational Acceptance Testing)
+// 1) Component/Unit/Module/Program testing
+// 2) Component integration testing
+// 3) System integration testing
+// 4) System testing
+// 5) Acceptance testing (User Acceptance Testing and Operational Acceptance Testing)
 
 // - Testing types
 // -- Functional (what the system should do - completeness, correctness, appropriateness)
